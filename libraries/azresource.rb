c59d607ddf990f11a95adb0b35af48a39ff4cfd5
ahelal <ahelal@users.noreply.github.com>
2020-04-29 22:09:43 +0200

    make resource group opitional in azresource

--- libraries/azresource.rb
+++ libraries/azresource.rb
@@ -5,7 +5,7 @@ require_relative 'azbasic'
 # AZ single resoruce
 class AzResource < AzBasic
   name 'azresource'
-  def initialize(subcommand, resource_name, resource_group)
+  def initialize(subcommand, resource_name, resource_group = nil)
     super(subcommand, 'show', resource_name, resource_group)
     @__resource_name__ = "#{@__resource_name__}: #{subcommand} #{resource_name}/#{resource_group}"
   end
