0338276fc49d35fe01b508e5f535da6c0235032e
ahelal <ahelal@users.noreply.github.com>
2020-06-08 21:56:25 +0200

    optional subscripition flag

--- libraries/azbasic.rb
+++ libraries/azbasic.rb
@@ -11,13 +11,14 @@ class AzBasic < Inspec.resource(1)
   desc 'Baisc parent resource should not be used directly'
   attr_reader :stdout, :stderr, :success
   include ObjectTraverser
-  def initialize(subcommand, list = nil, resource_name = nil, resource_group = nil, extra_args = nil)
+  def initialize(subcommand, list = nil, resource_name = nil, resource_group = nil, extra_args = nil, subscription = nil)
     raise Inspec::Exceptions::ResourceFailed, "A resource is needed. Can't be empty" if subcommand.to_s.length.zero?
 
     # TODO: disable any main commands
     # ['configure', 'feedback', 'find', 'interactive', 'login', 'logout', 'rest']
     azbin = ENV['AZCLI_BIN'] || 'az'
     command = "#{azbin} #{subcommand} #{list}"
+    command = "#{command} --subscription '#{subscription}'" if subscription
     command = "#{command} --name #{resource_name}" if resource_name
     command = "#{command} --resource-group #{resource_group}" if resource_group
     command = "#{command} #{extra_args}" if extra_args
--- libraries/azresource.rb
+++ libraries/azresource.rb
@@ -3,8 +3,8 @@
 # AZ single resoruce
 class AzResource < AzBasic
   name 'azresource'
-  def initialize(subcommand, resource_name, resource_group = nil, extra_args = nil)
-    super(subcommand, 'show', resource_name, resource_group, extra_args)
+  def initialize(subcommand, resource_name, resource_group = nil, extra_args = nil, subscription = nil)
+    super(subcommand, 'show', resource_name, resource_group, extra_args, subscription)
     @__resource_name__ = "#{@__resource_name__}: #{subcommand} -n #{resource_name}"
     @__resource_name__ = "#{@__resource_name__} -g #{resource_group}" if resource_group
   end
